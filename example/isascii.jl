cdd520da45777ca750fa9d46187d879bf5d7637a
Shun Sakai <sorairolake@protonmail.ch>
2023-06-02 01:00:31 +0900

    Add examples

---
+++ example/isascii.jl
@@ -0,0 +1,18 @@
+#
+# SPDX-License-Identifier: Apache-2.0 OR MIT
+#
+# Copyright (C) 2023 Shun Sakai
+#
+
+using Sysexits
+
+input = isempty(ARGS) ? readchomp(stdin) : ARGS[1]
+try
+    ascii(input)
+    println("OK")
+    Sysexits.exit(Sysexits.ok)
+catch err
+    showerror(stderr, err)
+    println(stderr)
+    Sysexits.exit(Sysexits.dataerr)
+end
